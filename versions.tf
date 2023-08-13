42f2cfd86f69147cea8ddc96a1fe9add59f88b32
kavisek-tw <kavi.sekhon@thoughtworks.com>
2023-08-13 11:11:40 -0700

    feat: update prefix

--- versions.tf
+++ versions.tf
@@ -11,7 +11,7 @@ terraform {
     hostname     = "app.terraform.io"
     organization = "twks-cohort"
     workspaces {
-      name = "team7-201-grafana-stack-prod"
+       prefix = "team7-201-grafana-stack-"
     }
   }
 }
